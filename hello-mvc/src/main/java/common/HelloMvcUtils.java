1d5be78b645957a497e1e69687db61642b03b0d9
45sjp <96190349+45sjp@users.noreply.github.com>
2022-05-23 19:24:29 +0900

    Add files via upload

--- hello-mvc/src/main/java/common/HelloMvcUtils.java
+++ hello-mvc/src/main/java/common/HelloMvcUtils.java
@@ -37,4 +37,75 @@ public class HelloMvcUtils {
 		Encoder encoder = Base64.getEncoder();
 		return encoder.encodeToString(encrypted);
 	}
+	
+	/**
+	 * pagebar
+	 * 	---------------------------------------------------
+	 * 	- prev 없음
+	 * 	----------------------------------------------------
+	 *  - pageNo 영역
+	 * 		<span class='cPage'>1</span>
+	 * 		<a href='/mvc/admin/memberList?cPage=2'>2</a>
+	 * 		<a href='/mvc/admin/memberList?cPage=3'>3</a>
+	 * 		<a href='/mvc/admin/memberList?cPage=4'>4</a>
+	 * 		<a href='/mvc/admin/memberList?cPage=5'>5</a>
+	 * 	----------------------------------------------------
+	 * 	 - next 영역
+	 * 		<a href='/mvc/admin/memberList?cPage=6'>next</a>
+	 * 	----------------------------------------------------
+	 * 
+	 * @param cPage
+	 * @param numPerPage 10
+	 * @param totalContents 115
+	 * @param url /mvc/admin/memberList
+	 * @return
+	 */
+	public static String getPagebar(int cPage, int numPerPage, int totalContents, String url) {
+		StringBuilder pagebar = new StringBuilder();
+		int totalPages = (int) Math.ceil((double) totalContents / numPerPage); // 전체 페이지 수
+		int pagebarSize = 5; // prev 1 2 3 4 5 next 이런 식
+		int pagebarStart = (cPage - 1) / pagebarSize * pagebarSize + 1; // 1, 6, 11
+		int pagebarEnd = pagebarStart + pagebarSize - 1; // 5, 10, 15
+		int pageNo = pagebarStart; // 증감변수는 pagebarStart부터 시작함
+		
+		// 링크에 제공될 주소
+		url += "?cPage=";
+		
+		// 이전 : prev
+		if(pageNo == 1) {
+			// prev버튼 비활성화
+		}
+		else {
+			// prev버튼 활성화
+			pagebar.append("<a href='" + url + (pageNo - 1) + "'>prev</a>");
+			pagebar.append("\n");
+		}
+		
+		// 번호
+		while(pageNo <= pagebarEnd && pageNo <= totalPages) {
+			if(pageNo == cPage) {
+				// 현재 페이지인 경우
+				pagebar.append("<span class='cPage'>" + pageNo + "</span>");
+				pagebar.append("\n");
+			}
+			else {
+				// 현재 페이지가 아닌 경우(링크 필요)
+				pagebar.append("<a href='" + url + pageNo + "'>" + pageNo + "</a>");
+				pagebar.append("\n");
+			}
+			pageNo++;
+		}
+		// 다음 : next
+		if(pageNo > totalPages) {
+			// 다음 페이지가 없는 경우
+		}
+		else {
+			// 다음 페이지가 있는 경우
+			pagebar.append("<a href='" + url + pageNo + "'>next</a>");
+			pagebar.append("\n");
+		}
+		
+		return pagebar.toString();
+	}
+	
 }
